26526a563c18149d2515f014b600f42a061b1f48
asdozzz <asdozzz89@gmail.com>
2018-07-16 22:44:16 +0500

    f

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -15,8 +15,12 @@ class Cors
      */
     public function handle($request, Closure $next)
     {
-        return $next($request)
-            ->header('Access-Control-Allow-Origin', '*')
-            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        if ($request->isMethod('options')) {
+            return response('', 200)
+                ->header('Access-Control-Allow-Methods', 'POST, GET, DELETE, OPTIONS, PUT')
+                ->header('Access-Control-Allow-Headers', 'accept, content-type, x-xsrf-token, x-csrf-token');
+        }
+
+        return $next($request);
     }
 }
