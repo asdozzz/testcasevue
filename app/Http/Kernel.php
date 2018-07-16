9c2690069cb504e77f3c00fa8a0ce3a93987587a
asdozzz <asdozzz89@gmail.com>
2018-07-16 22:25:17 +0500

    f

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -38,7 +38,7 @@ class Kernel extends HttpKernel
 
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            \App\Http\Middleware\Cors::class,
+            \Barryvdh\Cors\HandleCors::class,
         ],
 
         'api' => [
